28d258ea056f52152f8735210ca70cce2f72f33a
Dmitry Tomp <dmitrytomp@gmail.com>
2017-08-08 20:56:51 +0300

    Spectator View: Add ability to specify DSLR focal length and matrix dimensions in calibration (#174)
    * Add macro definitions
    
    * Add implementation
    
    * Add some debug output
    
    * Turn off the feature
    
    * Tabs to spaces
    
    * Add related calibration options

--- SpectatorView/Calibration/Calibration/CalibrationApp.cpp
+++ SpectatorView/Calibration/Calibration/CalibrationApp.cpp
@@ -379,11 +379,28 @@ void CalibrationApp::PerformCalibration()
     cv::Mat distCoeffColor, distCoeffHolo;
     cv::Mat colorR, holoR, colorT, holoT;
     
+#if DSLR_USE_KNOWN_INTRINSICS
+    double colorFocalLength = DSLR_FOCAL_LENGTH * std::min(HOLO_WIDTH / DSLR_MATRIX_WIDTH, HOLO_HEIGHT / DSLR_MATRIX_HEIGHT);
+    cv::Mat colorMat = (cv::Mat_<double>(3,3) << colorFocalLength, 0, HOLO_WIDTH / 2., 0, colorFocalLength, HOLO_HEIGHT / 2., 0, 0, 1);
+#else
     cv::Mat colorMat = cv::initCameraMatrix2D(colorObjectPoints, colorImagePoints, cv::Size(HOLO_WIDTH, HOLO_HEIGHT), (double)HOLO_HEIGHT / (double)HOLO_WIDTH);
+#endif
     cv::Mat holoMat = cv::initCameraMatrix2D(holoObjectPoints, holoImagePoints, cv::Size(HOLO_WIDTH, HOLO_HEIGHT), (double)HOLO_HEIGHT / (double)HOLO_WIDTH);
 
     OutputDebugString(L"Start Calibrating DSLR.\n");
-    double colorRMS = cv::calibrateCamera(colorObjectPoints, colorImagePoints, cv::Size(HOLO_WIDTH, HOLO_HEIGHT), colorMat, distCoeffColor, colorR, colorT, CV_CALIB_USE_INTRINSIC_GUESS);
+    int colorFlags = CV_CALIB_USE_INTRINSIC_GUESS;
+#if DSLR_USE_KNOWN_INTRINSICS
+    OutputDebugString(L"Setting user-defined focal length before calibration: ");
+    OutputDebugString(std::to_wstring(colorFocalLength).c_str());
+    OutputDebugString(L"\n");
+#if DSLR_FIX_FOCAL_LENGTH
+    colorFlags |= CV_CALIB_FIX_FOCAL_LENGTH;
+#endif
+#if DSLR_FIX_PRINCIPAL_POINT
+    colorFlags |=  CV_CALIB_FIX_PRINCIPAL_POINT;
+#endif
+#endif
+    double colorRMS = cv::calibrateCamera(colorObjectPoints, colorImagePoints, cv::Size(HOLO_WIDTH, HOLO_HEIGHT), colorMat, distCoeffColor, colorR, colorT, colorFlags);
     OutputDebugString(L"Done Calibrating DSLR.\n");
     OutputDebugString(L"Start Calibrating HoloLens.\n");
     double holoRMS = cv::calibrateCamera(holoObjectPoints, holoImagePoints, cv::Size(HOLO_WIDTH, HOLO_HEIGHT), holoMat, distCoeffHolo, holoR, holoT, CV_CALIB_USE_INTRINSIC_GUESS);
--- SpectatorView/Calibration/Calibration/stdafx.h
+++ SpectatorView/Calibration/Calibration/stdafx.h
@@ -23,6 +23,18 @@
 
 #define CALIBRATION_FREQUENCY_SECONDS 3
 
+// Known DSLR intrinsics (enable only if you can provide all these values)
+#define DSLR_USE_KNOWN_INTRINSICS FALSE
+#define DSLR_FOCAL_LENGTH 0.014
+#define DSLR_MATRIX_WIDTH 0.036
+#define DSLR_MATRIX_HEIGHT 0.024
+// Set to true if you want calibration app to avoid adjustment of
+// supplied focal length and/or matrix dimensions
+#define DSLR_FIX_FOCAL_LENGTH FALSE
+// Set to true if you are sure that optical axis of your lens
+// intersects with matrix at its center
+#define DSLR_FIX_PRINCIPAL_POINT FALSE
+
 // Use the C++ standard templated min/max
 #define NOMINMAX
 
