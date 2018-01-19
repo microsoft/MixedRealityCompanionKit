ae8db701990be9af22ba4c84ace77b5d8040a77d
Kevin Kennedy <6423774+KevinKennedy@users.noreply.github.com>
2018-01-19 14:55:44 -0800

    Add copyright to Settings.cs file

--- Bundler/Processor/ProcessorService/AssetCreator/Settings.cs
+++ Bundler/Processor/ProcessorService/AssetCreator/Settings.cs
@@ -1,4 +1,6 @@
-﻿namespace AssetCreator {
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+namespace AssetCreator {
     
     
     // This class allows you to handle specific events on the settings class:
